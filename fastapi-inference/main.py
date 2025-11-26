3cbdea25da4249ed94f3f262d82e2e797d99c80c
Janina Mattes <janina.alica.mattes@gmail.com>
2025-11-26 14:19:31 +0000

    src: Add dummy endpoints

--- fastapi-inference/main.py
+++ fastapi-inference/main.py
@@ -1,9 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from typing import Annotated
 
-app = FastAPI()
+
+
+app = FastAPI(
+    title="LatentFlow Inference API",
+    description="API for controlled generative image manipulation using VAEs and Flow-Matching models.",
+    version="1.0.0"
+)
+
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
+@app.post("/files/")
+async def create_file(file: Annotated[bytes, File()]):
+    return {"file_size": len(file)}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
