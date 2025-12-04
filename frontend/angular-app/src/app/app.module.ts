b95ffd93d361d7be66c838a098555fd5f615cc92
Janina Mattes <janina.alica.mattes@gmail.com>
2025-12-04 21:51:28 +0000

    src: Update nav bar

--- frontend/angular-app/src/app/app.module.ts
+++ frontend/angular-app/src/app/app.module.ts
@@ -5,22 +5,29 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { CardsComponent } from './cards/cards.component';
 import { HomeComponent } from './home/home.component';
+import { ContentComponent } from './content/content.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { GalleriaComponent } from './galleria/galleria.component';
+import { ImageCompareComponent } from './image-compare/image-compare.component';
+import { SliderThumbComponent } from './slider-thumb/slider-thumb.component';
 
 export function createTranslationLoader(http: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(http, '/assets/i18n/', '.json');
 }
 
 const modules = [
+  MatSliderModule,
   BrowserModule,
   BrowserAnimationsModule,
   HttpClientModule,
-  SharedModule,
+  SharedModule, // All shared modules
   LoggerModule.forRoot({
     level: NgxLoggerLevel.TRACE, // remove later just for debugging
     disableConsoleLogging: false
@@ -40,7 +47,12 @@ const modules = [
   declarations: [
     AppComponent,
     CardsComponent,
-    HomeComponent
+    HomeComponent,
+    ContentComponent,
+    CarouselComponent,
+    GalleriaComponent,
+    ImageCompareComponent,
+    SliderThumbComponent
   ],
   imports: modules,
   providers: [],
