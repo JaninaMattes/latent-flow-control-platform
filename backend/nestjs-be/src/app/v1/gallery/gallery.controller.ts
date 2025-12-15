e179e6a4d2ec30eaffe296f7d27d7ca192661f62
Janina Mattes <janina.alica.mattes@gmail.com>
2025-12-15 23:08:45 +0000

    src: Update backend endpoint to extend controller

--- backend/nestjs-be/src/app/v1/gallery/gallery.controller.ts
+++ backend/nestjs-be/src/app/v1/gallery/gallery.controller.ts
@@ -1,8 +1,19 @@
-import { Controller, Get, Logger, UseGuards, Query } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  Controller,
+  Get,
+  Logger,
+  UseGuards,
+  Query,
+  Param,
+} from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JWTAuthGuard } from '../Google-auth/guards/jwt-auth.guard';
 import { GalleryService } from './gallery.service';
-import { ImageContentDto } from './dto/image-content.dto';
+import {
+  CategoryImageDto,
+  ImageCategoryDto,
+  ImageContentDto,
+} from './dto/image-content.dto';
 
 @ApiTags('Gallery')
 @Controller('gallery')
@@ -11,6 +22,12 @@ export class GalleryController {
 
   constructor(private readonly galleryService: GalleryService) {}
 
+  /**
+   * Fetches all generated content.
+   * @param limit
+   * @param offset
+   * @returns
+   */
   @ApiOperation({ summary: 'Retrieve all generated images.' })
   @ApiResponse({ status: 200, type: [ImageContentDto] })
   @UseGuards(JWTAuthGuard)
@@ -19,7 +36,41 @@ export class GalleryController {
     @Query('limit') limit = 10,
     @Query('offset') offset = 0,
   ): Promise<ImageContentDto[]> {
-    this.logger.log(`Fetching generated images: limit=${limit}, offset=${offset}`);
-    return this.galleryService.getGeneratedImages(Number(limit), Number(offset));
+    this.logger.log(
+      `Fetching generated images: limit=${limit}, offset=${offset}`,
+    );
+    return this.galleryService.getGeneratedImages(
+      Number(limit),
+      Number(offset),
+    );
+  }
+
+  /**
+   * Fetches all categories of images.
+   * @returns
+   */
+  @ApiOperation({ summary: 'Retrieve all generated images.' })
+  @ApiResponse({ status: 200, type: [ImageCategoryDto] })
+  @UseGuards(JWTAuthGuard)
+  @Get('/categories')
+  async getAllCategoriesContent(): Promise<ImageCategoryDto[]> {
+    this.logger.log(`Fetching all image categories.`);
+    return this.galleryService.getImageCategories();
+  }
+
+  /**
+   * Fetches all images per category.
+   * @returns
+   */
+  @ApiOperation({ summary: 'Retrieve all images by category ID.' })
+  @ApiParam({ name: 'categoryId', description: 'Category ID' })
+  @ApiResponse({ status: 200, type: [CategoryImageDto] })
+  @UseGuards(JWTAuthGuard)
+  @Get('categories/:categoryId/images')
+  async getAllImagesByCategory(
+    @Param('categoryId') categoryId: string,
+  ): Promise<CategoryImageDto[]> {
+    this.logger.log(`Fetching images for category ID: ${categoryId}`);
+    return this.galleryService.getImagesByCategory(categoryId);
   }
 }
