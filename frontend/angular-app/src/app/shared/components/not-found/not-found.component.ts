0d697368ccd2b1d4a5b817993cdd657263600cf1
Janina Mattes <janina.alica.mattes@gmail.com>
2025-12-30 21:01:48 +0000

    src: Add a page-not-found error page for angular routing

--- frontend/angular-app/src/app/shared/components/not-found/not-found.component.ts
+++ frontend/angular-app/src/app/shared/components/not-found/not-found.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
+  standalone: true,
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.sass']
 })
-export class NotFoundComponent implements OnInit {
+export class NotFoundComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
