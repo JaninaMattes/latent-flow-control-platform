4a0defe4c10a72c7ca333ae779d0ef1ff65935d3
Janina Alica Mattes <janina.alica.mattes@gmail.com>
2026-04-06 23:33:55 +0200

    src: Uncommented for current state

--- backend/fast-api/app/infrastructure/loader/data_loader.py
+++ backend/fast-api/app/infrastructure/loader/data_loader.py
@@ -1,23 +1,18 @@
+# def load_data(data_path: str, batch_size: int, source_time)
+#     """
+#     Load the data in the required format.
+#     """
+#     data = HDF5DataModule(
+#             hdf5_file=data_path,
+#             batch_size=batch_size,
+#             source_timestep=source_timestep,
+#             target_timestep=target_timestep,
+#             num_workers=4,
+#             train=False,
+#             validation=(group == "validation"),
+#             test=(group == "test"),
+#             group_name=group
+#         )
+# data.setup(stage="fit" if group == "validation" else "test")
 
-
-from backend.packages.ldm.ldm.dataloader.hdf5_dataset.hdf5_dataloader import HDF5DataModule
-
-
-def load_data(data_path: str, batch_size: int, source_time)
-    """
-    Load the data in the required format.
-    """
-    data = HDF5DataModule(
-            hdf5_file=data_path,
-            batch_size=batch_size,
-            source_timestep=source_timestep,
-            target_timestep=target_timestep,
-            num_workers=4,
-            train=False,
-            validation=(group == "validation"),
-            test=(group == "test"),
-            group_name=group
-        )
-data.setup(stage="fit" if group == "validation" else "test")
-
-return data
+# return data
